6ab8447e047157abbe1a28e9756c0401a16bba97
Alexis <amonroy-2023021@kinal.edu.gt>
2024-06-13 22:47:34 -0600

    Instalacion de Apis y Reportes

--- src/org/AlexisMonroy/report/GenerarReportes.java
+++ src/org/AlexisMonroy/report/GenerarReportes.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.AlexisMonroy.report;
 
-/**
- *
- * @author informatica
- */
+import java.io.InputStream;
+import java.util.Map;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+import org.AlexisMonroy.DB.Conexion;
+
 public class GenerarReportes {
     
+    public static void mostrarReportes(String nombreReporte, String titulo, Map parametro){
+        InputStream reporte = GenerarReportes.class.getResourceAsStream(nombreReporte);
+        try{
+            JasperReport reporteMaestro = (JasperReport)JRLoader.loadObject(reporte);
+            JasperPrint reporteImpreso = JasperFillManager.fillReport(reporteMaestro, parametro, Conexion.getInstance().getConexion());
+            JasperViewer visor = new JasperViewer(reporteImpreso, false);
+            visor.setTitle(titulo);
+            visor.setVisible(true);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    
+    }
 }
